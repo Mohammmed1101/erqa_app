55697c3ef86e87a299f853301aa95aa0aa280e4f
Mohammmed1101 <92180726+Mohammmed1101@users.noreply.github.com>
2022-06-14 21:04:19 +0300

    create file requirements.txt

--- erqaApp/users/views.py
+++ erqaApp/users/views.py
@@ -1,8 +1,12 @@
+import django
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.contrib.auth.models import User
+from rest_framework import viewsets , serializers
 
 # Create your views here.
+class UserSeriaalizer(serializers.ModelSerializer):
+    pass
 def signup(request):
     return HttpResponse('Sign Up page')
 
