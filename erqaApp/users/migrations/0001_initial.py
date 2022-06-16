37366f1aed2472712487286d6d6f93c54a2b91a6
Mohammmed1101 <92180726+Mohammmed1101@users.noreply.github.com>
2022-06-16 20:07:06 +0300

    Update 0001_initial.py

--- erqaApp/users/migrations/0001_initial.py
+++ erqaApp/users/migrations/0001_initial.py
@@ -11,17 +11,7 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.CreateModel(
-            name='Employee',
-            fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
-                ('eid', models.CharField(max_length=20)),
-                ('ename', models.CharField(max_length=100)),
-                ('eemail', models.EmailField(max_length=254)),
-                ('econtact', models.CharField(max_length=15)),
-            ],
-            options={
-                'db_table': 'employee',
-            },
-        ),
+        # migrations.CreateModel(
+    
+        # ),
     ]
